ac4b70256a915cee69ecc77d5b9867f0a44b0002
ithan <ithan.camacho@epn.edu.ec>
2025-01-12 18:14:25 -0500

    Rutas Privadas

--- src/routers/rutas_routes.js
+++ src/routers/rutas_routes.js
@@ -17,9 +17,9 @@ import { listarParadas } from '../controllers/parada_controller.js';
 
 router.get("/rutas",  listarRutas);
 router.get("/ruta/:id",  detalleRuta);
-router.post("/ruta/registro",  registrarRuta);
-router.put("/ruta/actualizar/:id",  actualizarRuta);
-router.delete("/ruta/eliminar/:id",  eliminarRuta);
+router.post("/ruta/registro", verificarAutenticacion, registrarRuta);
+router.put("/ruta/actualizar/:id", verificarAutenticacion, actualizarRuta);
+router.delete("/ruta/eliminar/:id", verificarAutenticacion, eliminarRuta);
 router.get("corredor/rutas/:corredorId", listarRutasPorCorredor);
 router.get("/ruta/paradas/:id", listarParadasDeRuta);
 
